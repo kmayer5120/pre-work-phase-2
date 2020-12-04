6eea8be26001acd597f5b1907b51046bfce91c1c
Kyle Mayer <kmayer1@cnm.edu>
2020-12-04 10:23:33 -0700

    Adding file for variables and types video lesson.

---
+++ fundamentals/variables-and-data.js
@@ -0,0 +1,15 @@
+/*
+ * Variables, Data Types, and Typing
+ */
+
+let obj = {
+    key1: "value",
+    key2: 4,
+    boolean: true,
+    obj2: {
+        obj2Key1: "Internal Object Value",
+    }
+}
+
+console.log(obj.key1)
+
