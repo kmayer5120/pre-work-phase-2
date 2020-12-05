f1fcfcca98ec9346e63de2a52e90effb8b4a45b4
Kyle Mayer <kmayer1@cnm.edu>
2020-12-05 09:41:20 -0700

    Completed part 2 of shopping list. (Forgot to commit and push this yesterday!)

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -16,19 +16,44 @@ function handleItemForm(e, formRef) {
     }
 
     addItemToShoppingList();
+    formRef.reset();
     return false;
 }
 
 function addItemToShoppingList() {
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount");
-    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+    let id = getRandomInt(0, 10000000);
+
+    //creates list item html and appends to page
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
     let itemListRef = document.getElementById("shopping-list");
     itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+    setDeleteButtonEvent(id);
 }
 
-function createListItemHtml(itemName, itemAmount) {
-    return `<li> 
+function setDeleteButtonEvent(id) {
+    let deleteButton = document.getElementById("button"+id);
+    deleteButton.addEventListener('click', () => {
+        removeListItem(id);
+    });
+}
+
+function createListItemHtml(itemName, itemAmount, id) {
+    return `<li id="item${id}"> 
                 ${itemName} - ${itemAmount}
+                <button id="button${id}">Delete Item</button>
             </li>`;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function removeListItem(id) {
+    let listItem = document.getElementById("item"+id);
+    listItem.parentNode.removeChild(listItem);
 }
